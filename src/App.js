ffd7eff8309336da648108407203ed92789eedb8
heejeong Jang <mae04038@naver.com>
2022-08-08 23:44:36 +0900

    second

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import SubPage from "./components/SubPage/SubPage";
+
+function App() {
+  return (
+    <div className="App">
+      <SubPage />
+    </div>
+  );
+}
+
+export default App;
